1d89040e805c135f538b7687e6157fa441d55b82
Bart Wiegmans <bartwiegmans@gmail.com>
2012-08-19 16:47:10 +0200

    Added app route

--- module/mod_parrot_route.c
+++ module/mod_parrot_route.c
@@ -1,12 +1,36 @@
 #include "mod_parrot.h"
+#include <sys/stat.h> 
+#include <libgen.h>
 
 extern module mod_parrot;
 
+/* not portable! */
+int file_exists(char * path) {
+    struct stat buf;
+    return !stat(path, &buf);
+}
+
 mod_parrot_route * app_route(request_rec * req, mod_parrot_spec * spec) {
-    /* we really should have some way to determine when we should handle it */
+    mod_parrot_route * route;
+    /* handle it if we have a specified application and the file does not exist 
+     * or the file requested is the specified script for the application */
     if(!spec->application)
         return NULL;
-    return NULL;
+    if(file_exists(req->filename) && 
+       strcmp(req->filename, spec->application->script))
+        return NULL; /* other file than the requested script, 
+                        we shouldn't handle it */
+    route = apr_pcalloc(req->pool, sizeof(mod_parrot_route));
+    route->language = spec->application->language;
+    if(spec->application->script) { /* append the script to the directory to find the relative path */
+        char * path = apr_pstrdup(req->pool, req->filename);
+        route->script = apr_pstrcat(req->pool, dirname(path), "/", spec->application->script, NULL);
+    } else { /* use the filename */
+        route->script = req->filename;
+    }
+    route->className = spec->application->className;
+    route->routine = spec->application->routine;
+    return route;
 }
 
 /* mime routing is really fallback routing as far as i'm concerned */
@@ -62,10 +86,12 @@ mod_parrot_route * mod_parrot_parse_route(apr_pool_t * pool, const char * arg) {
     if(apr_uri_parse(pool, arg, &uri) != APR_SUCCESS) 
         return NULL;
     route = apr_pcalloc(pool, sizeof(mod_parrot_route));
-    route->language = uri.scheme;
+    route->language = uri.scheme; 
     route->script = uri.hostname; /* this is relative */
-    route->className = (uri.path && strlen(uri.path) > 1) ? uri.path : NULL;
-    route->routine = uri.fragment;
+    /* ignore the first slash */
+    route->className = (uri.path && strlen(uri.path) > 1) ? (uri.path + 1) : NULL;
+    /* and the hash of the fragment */
+    route->routine = (uri.fragment && strlen(uri.fragment) > 1) ? (uri.fragment + 1) : NULL;
     return route;
 }
 
